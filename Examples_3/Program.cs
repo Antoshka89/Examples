24cfe364f21796046cf967b9fe4dc58966ef7314
Anton Zhidyaev <zhidyaev.anton@gmail.com>
2023-10-11 18:27:56 +0500

    Добавили рандом

--- Examples_3/Program.cs
+++ Examples_3/Program.cs
@@ -1,4 +1,11 @@
-﻿int numberA = 32;
-int numberB = 12;
+﻿// int numberA = 32;
+// int numberB = 12;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+int numberA = new Random(). Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random(). Next(1, 100);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
