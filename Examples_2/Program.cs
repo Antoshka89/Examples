99a1122e5e773bcdd6d030ef4fa7729443e796da
Anton Zhidyaev <zhidyaev.anton@gmail.com>
2023-10-11 17:47:43 +0500

    add Examples 2

---
+++ Examples_2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите ваше имя:");
+string username = Console.ReadLine()!;
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
+
+// string username = Console.ReadLine()!; ! убирает ошибку Преобразование литерала, допускающего значение
+// NULL или возможного значения NULL в тип, не допускающий значение NULL.
+
+// console.Write Выводит в одну строчку
+// пример:
+// Console.Write("Введите ваше имя:");
+// string username = Console.ReadLine()!;
+// Console.Write("Привет,");
+// Console.Write(username);
